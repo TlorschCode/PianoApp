0bdf082b0ce6e391908bc2f4fed2fe9d072bfeaf
Thomas <thomas.lorsch@gmail.com>
2025-07-06 15:09:23 -0700

    Made a commit.bat to push changes to GitHub

---
+++ commit.bat
@@ -0,0 +1,19 @@
+@echo off
+REM Check if commit message argument is provided
+if "%~1"=="" (
+    echo Please provide a commit message.
+    echo Usage: gitcommit "Your commit message"
+    exit /b 1
+)
+
+REM Add all changes
+git add .
+
+REM Commit with the passed message
+git commit -m "%~1"
+
+REM Push to the current branch
+git push
+
+echo Commit and push done!
+pause
