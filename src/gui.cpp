ed8ba2245abae6acc8666b5e8371de88b289ff56
Thomas <thomas.lorsch@gmail.com>
2025-08-19 15:50:15 -0700

    Resized assets

--- src/gui.cpp
+++ src/gui.cpp
@@ -25,7 +25,6 @@ static mt19937 gen(rd()); // random number generator
 
 //| ENUMERATORS
 enum ButtonType {SQUARE, CIRCLE};
-enum ButtonGroup {DEFAULT, SETTINGS};
 enum MenuState {FLASHCARD_MENU, SETTINGS_MENU};
 
 //| Template functions
@@ -69,7 +68,7 @@ class Button {
             available_states = btn_available_states;
             selected = false;
             if (type == SQUARE) {
-                rect = {pos.x - (size / 2), pos.y - (size / 2), size, size};
+                rect = {pos.x - size, pos.y - size, size, size};
             }
         }
         void SetOcapacity(float target_amnt) {
@@ -132,15 +131,6 @@ int sizeTrebleLines = sizeof(trebleLines) / sizeof(trebleLines[0]);
 int sizeTrebleSpaces = sizeof(trebleSpaces) / sizeof(trebleSpaces[0]);
 int sizeBaseLines = sizeof(baseLines) / sizeof(baseLines[0]);
 int sizeBaseSpaces = sizeof(baseSpaces) / sizeof(baseSpaces[0]);
-// For note rendering
-float noteStep = 40.9;
-int sharpXOffset = -26;
-int flatXOffset = -9;
-int flatYOffset = 15;
-int naturalXOffset = -20;
-int naturalYOfffset = 45;
-int ledgerXOffset = 28;
-int noteYOffset = 45;
 // Timers and frames
 int newNoteTimer = 0;
 int checkNoteTimer = 0;
@@ -221,44 +211,64 @@ inline int randint(int min, int max) {
 // Loads assets
 void loadAssets() {
     roboto = LoadFontEx("assets/Roboto-Black.ttf", 128, NULL, 0);
+    float relativeSizeY = 0;
+    float relativeSizeX = 0;
+    float standardSize = 100;
 
     Image tempImage = LoadImage("assets/GrandStaff.png");
-    ImageResize(&tempImage, tempImage.width * 2, tempImage.height * 2);
+    relativeSizeX = tempImage.width * (standardSize / tempImage.width);
+    relativeSizeY = tempImage.height * (standardSize / tempImage.width);
+    ImageResize(&tempImage, relativeSizeX * 18, relativeSizeY * 18);
     grandStaffTexture = LoadTextureFromImage(tempImage);
     UnloadImage(tempImage);
 
     tempImage = LoadImage("assets/sharp.png");
-    ImageResize(&tempImage, tempImage.width / 9, tempImage.height / 9);
+    relativeSizeX = tempImage.width * (standardSize / tempImage.width);
+    relativeSizeY = tempImage.height * (standardSize / tempImage.width);
+    ImageResize(&tempImage, relativeSizeX / 4, relativeSizeY / 4);
     sharpTexture = LoadTextureFromImage(tempImage);
     UnloadImage(tempImage);
 
     tempImage = LoadImage("assets/Flat.png");
-    ImageResize(&tempImage, tempImage.width / 9.7, tempImage.height / 9.7);
+    relativeSizeX = tempImage.width * (standardSize / tempImage.width);
+    relativeSizeY = tempImage.height * (standardSize / tempImage.width);
+    ImageResize(&tempImage, relativeSizeX / 4, relativeSizeY / 4);
     flatTexture = LoadTextureFromImage(tempImage);
     UnloadImage(tempImage);
 
     tempImage = LoadImage("assets/natural.png");
-    ImageResize(&tempImage, tempImage.width / 8, tempImage.height / 8);
+    relativeSizeX = tempImage.width * (standardSize / tempImage.width);
+    relativeSizeY = tempImage.height * (standardSize / tempImage.width);
+    ImageResize(&tempImage, relativeSizeX / 4, relativeSizeY / 4);
     naturalTexture = LoadTextureFromImage(tempImage);
     UnloadImage(tempImage);
 
     tempImage = LoadImage("assets/QuarterNote.png");
-    ImageResize(&tempImage, tempImage.width / 1.4, tempImage.height / 1.4);
+    relativeSizeX = tempImage.width * (standardSize / tempImage.width);
+    relativeSizeY = tempImage.height * (standardSize / tempImage.width);
+    ImageResize(&tempImage, relativeSizeX / 2, relativeSizeY / 2);
     noteTexture = LoadTextureFromImage(tempImage);
     UnloadImage(tempImage);
 
+    // TODO: Scale ledger line correctly
     tempImage = LoadImage("assets/LedgerLine.png");
-    ImageResize(&tempImage, tempImage.width / 2.5, tempImage.height / 2);
+    relativeSizeX = tempImage.width * (standardSize / tempImage.width);
+    relativeSizeY = tempImage.height * (standardSize / tempImage.width);
+    ImageResize(&tempImage, relativeSizeX, relativeSizeY);
     ledgerTexture = LoadTextureFromImage(tempImage);
     UnloadImage(tempImage);
 
     tempImage = LoadImage("assets/SettingsIcon.png");
-    ImageResize(&tempImage, tempImage.width / 5, tempImage.height / 5);
+    relativeSizeX = tempImage.width * (standardSize / tempImage.width);
+    relativeSizeY = tempImage.height * (standardSize / tempImage.width);
+    ImageResize(&tempImage, relativeSizeX / 1.5, relativeSizeY / 1.5);
     settingsTexture = LoadTextureFromImage(tempImage);
     UnloadImage(tempImage);
 
     tempImage = LoadImage("assets/CloseButton.png");
-    ImageResize(&tempImage, tempImage.width / 1.792, tempImage.height / 1.792);
+    relativeSizeX = tempImage.width * (standardSize / tempImage.width);
+    relativeSizeY = tempImage.height * (standardSize / tempImage.width);
+    ImageResize(&tempImage, relativeSizeX / 1.5, relativeSizeY / 1.5);
     closeSettingsTexture = LoadTextureFromImage(tempImage);
     UnloadImage(tempImage);
 }
@@ -303,13 +313,18 @@ void drawStaff(Texture2D txtr) {
 // Displays whichever note is passed into it.
 // The tint of the textures is changeable, but defaults to White.
 void drawNote(string useNote, Color tint = WHITE) {
-    // 40.9 pixels between each line
+    // 44.5 pixels between each line
     // 495 is the bottom line for Treble Clef, or E4
-    // 785 is the bottom line for Base Clef, or G2
-    //# Note Decoding
+    // 803 is the bottom line for Base Clef, or G2
     int y = 0;
     int x = 500;
+    float noteStep = 44.5;
+    int accidentalXOffset = -30;
+    int accidentalYOffset = -5;
+    int ledgerXOffset = 0;
+    int noteYOffset = 45;
 
+    //# Note Decoding
     string note = (useNote.length() == 3) ? (useNote.substr(0, 1) + useNote.back()) : useNote;
     char accidental = (useNote.length() == 3) ? useNote.at(1) : '\0';
     bool isTrebleLine = indexOf(note, trebleLines) != nullopt;
@@ -321,7 +336,7 @@ void drawNote(string useNote, Color tint = WHITE) {
 
     //# Note Rendering
     if (isTreble) {
-        y = isTrebleLine ? 535 - (noteStep * indexOf(note, trebleLines).value_or(-1)) : 515 - (noteStep * indexOf(note, trebleSpaces).value_or(-1));
+        y = isTrebleLine ? 538 - (noteStep * indexOf(note, trebleLines).value_or(-1)) : 513 - (noteStep * indexOf(note, trebleSpaces).value_or(-1));
         if (note == "C4") {
             DrawTexture(ledgerTexture, x + ledgerXOffset, y - (ledgerTexture.height / 2), tint);
         }
@@ -329,29 +344,29 @@ void drawNote(string useNote, Color tint = WHITE) {
         if (accidental != '\0') {
             switch (accidental) {
                 case '#':
-                    DrawTexture(sharpTexture, x + sharpXOffset, y - (sharpTexture.height / 2), tint);
+                    DrawTexture(sharpTexture, x + accidentalXOffset, y - ((sharpTexture.height / 2) + accidentalYOffset), tint);
                     break;
                 case 'b':
-                    DrawTexture(flatTexture, x + flatXOffset, y - ((flatTexture.height / 2) + flatYOffset), tint);
+                    DrawTexture(flatTexture, x + accidentalXOffset, y - ((flatTexture.height / 2) + accidentalYOffset), tint);
                     break;
                 case 'N':
-                    DrawTexture(naturalTexture, x + naturalXOffset,  y - ((naturalTexture.height / 2) + naturalYOfffset), tint);
+                    DrawTexture(naturalTexture, x + accidentalXOffset,  y - ((naturalTexture.height / 2) + accidentalYOffset), tint);
                     break;
             }
         }
     } else if (isBase) {  //# Base Clef
-        y = isBaseLine ? 785 - (noteStep * indexOf(note, baseLines).value_or(-1)) :  805 - (noteStep * indexOf(note, baseSpaces).value_or(-1));
+        y = isBaseLine ? 800 - (noteStep * indexOf(note, baseLines).value_or(-1)) :  822 - (noteStep * indexOf(note, baseSpaces).value_or(-1));
         DrawTexture(noteTexture, x, y - ((noteTexture.height / 2) + noteYOffset), tint);
         if (accidental != '\0') {
             switch (accidental) {
                 case '#':
-                    DrawTexture(sharpTexture, x + sharpXOffset, y - (sharpTexture.height / 2), tint);
+                    DrawTexture(sharpTexture, x + accidentalXOffset, y - ((sharpTexture.height / 2) + accidentalYOffset), tint);
                     break;
                 case 'b':
-                    DrawTexture(flatTexture, x + flatXOffset, y - ((flatTexture.height / 2) + flatYOffset), tint);
+                    DrawTexture(flatTexture, x + accidentalXOffset, y - ((flatTexture.height / 2) + accidentalYOffset), tint);
                     break;
                 case 'N':
-                    DrawTexture(naturalTexture, x + naturalXOffset,  y - ((naturalTexture.height / 2) + naturalYOfffset), tint);
+                    DrawTexture(naturalTexture, x + accidentalXOffset,  y - ((naturalTexture.height / 2) + accidentalYOffset), tint);
                     break;
             }
         }
@@ -436,8 +451,11 @@ void drawButtons() {
 }
 
 bool mouseLogic(string *hovered) {
-    *hovered = "X";
+    *hovered = "???";
     Vector2 mouse = GetMousePosition();
+    if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)) {
+        cout << ">>>> DEBUG: " << mouse.x << ", " << mouse.y << "\n";
+    }
     for (Button &button : buttons) {
         if (button.IsHovered(mouse)) {
             *hovered = button.name;
@@ -481,17 +499,17 @@ void RunGUI() {
     SetTextLineSpacing(16);
     loadAssets();
 
-    Button settingsButton("settings", Vector2({WINWIDTH - 50, 45}), 50, CIRCLE, settingsTexture, vector<MenuState> {FLASHCARD_MENU});
+    Button settingsButton("settings", Vector2({WINWIDTH - 40, 80}), 50, CIRCLE, settingsTexture, vector<MenuState> {FLASHCARD_MENU});
     buttons.push_back(settingsButton);
-    Button closeSettingsButton("close_settings", Vector2({WINWIDTH - 50, 45}), 50, SQUARE, closeSettingsTexture, vector<MenuState> {SETTINGS_MENU});
+    Button closeSettingsButton("close_settings", Vector2({WINWIDTH - 40, 80}), 50, CIRCLE, closeSettingsTexture, vector<MenuState> {SETTINGS_MENU});
     buttons.push_back(closeSettingsButton);
     // TODO: Make this an actual button, not just a texture of a flat.
-    Button sharpButton("do_sharps", Vector2({WINWIDTH - 500, 90}), 50, CIRCLE, flatTexture, vector<MenuState> {SETTINGS_MENU});
+    Button sharpButton("do_sharps", Vector2({WINWIDTH - 500, 90}), 50, CIRCLE, sharpTexture, vector<MenuState> {SETTINGS_MENU});
     buttons.push_back(sharpButton);
 
     string hoveredBtn = "???";
     newNoteTimer = 0;
-    string correctNote = "F#3";
+    string correctNote = "G#2";
     bool was_clicked = false;
 
     while (!WindowShouldClose()) {
